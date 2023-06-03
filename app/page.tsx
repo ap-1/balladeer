75b7871cf17cffcb0cf01c6a805b7564fbb02d01
Anish Pallati <anishp@duck.com>
2023-06-03 17:30:26 -0400

    feat: Fix SVG coloring in dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -56,8 +56,7 @@ export default function Home() {
 			<Navbar currentTitle="Home" />
 			<Content
 				as="heading"
-				outerClassName="py-8"
-				className="flex flex-col gap-3 mt-16 sm:gap-0 sm:flex-row"
+				className="flex flex-col gap-3 my-16 sm:gap-0 sm:flex-row"
 			>
 				<div className="flex items-center max-w-lg">
 					<div className="flex flex-col gap-y-3">
@@ -69,14 +68,13 @@ export default function Home() {
 							minutes with AI-powered text summarization.
 						</h2>
 
-						<div className="flex items-center w-full max-w-full p-2 mt-3 space-x-2 rounded-md sm:max-w-sm bg-muted">
-							<BookMarked className="w-10 h-10" />
+						<div className="flex items-center w-full max-w-full mt-3 space-x-2 sm:max-w-sm">
 							<Input
 								type="text"
 								placeholder="Summarize a book right now!"
 							/>
 							<Button type="submit" onClick={generate}>
-								<Send className="w-5 h-5" />
+								<BookMarked className="w-5 h-5" />
 							</Button>
 						</div>
 					</div>
@@ -85,13 +83,17 @@ export default function Home() {
 				<Image
 					src="/images/student-going-to-school.svg"
 					alt="student-going-to-school"
-					className="mx-auto"
+					className="mx-auto dark:invert"
 					height={400}
 					width={400}
 				></Image>
 			</Content>
 
-			<Content as="section" outerClassName="mt-16">
+			<Content
+				as="section"
+				className="mt-16"
+				outerClassName="border-t border-border"
+			>
 				<h1 className="flex justify-center mx-auto font-sans text-4xl font-black">
 					How it Works
 				</h1>
@@ -103,11 +105,11 @@ export default function Home() {
 							alt="search"
 							height={300}
 							width={300}
-							className="mx-auto "
+							className="mx-auto dark:invert"
 						></Image>
 
 						<div className="flex flex-row items-center">
-							<div className="flex items-center justify-center w-8 h-8 text-white bg-orange-500 rounded-md">
+							<div className="flex items-center justify-center w-8 h-8 text-white bg-orange-500 rounded-md dark:bg-sky-500">
 								1
 							</div>
 							<div className="ml-4 text-lg font-semibold ">
@@ -124,13 +126,13 @@ export default function Home() {
 						<Image
 							src="/images/surreal-hourglass.svg"
 							alt="surreal-hourglass"
-							className="mx-auto"
+							className="mx-auto dark:invert"
 							height={300}
 							width={300}
 						></Image>
 
 						<div className="flex flex-row items-center">
-							<div className="flex items-center justify-center w-8 h-8 text-white bg-orange-500 rounded-md">
+							<div className="flex items-center justify-center w-8 h-8 text-white bg-orange-500 rounded-md dark:bg-sky-500">
 								2
 							</div>
 							<div className="ml-4 text-lg font-semibold">
@@ -145,14 +147,14 @@ export default function Home() {
 					<div className="flex flex-col items-center">
 						<Image
 							src="/images/work-from-home.svg"
-							className="mx-auto"
+							className="mx-auto dark:invert"
 							alt="work-from-home"
 							height={300}
 							width={300}
 						></Image>
 
 						<div className="flex flex-row items-center">
-							<div className="flex items-center justify-center w-8 h-8 text-white bg-orange-500 rounded-md">
+							<div className="flex items-center justify-center w-8 h-8 text-white bg-orange-500 rounded-md dark:bg-sky-500">
 								3
 							</div>
 							<div className="ml-4 text-lg font-semibold">
@@ -171,7 +173,7 @@ export default function Home() {
 				className="px-4 py-16 mx-auto text-center max-w-7xl sm:px-6 lg:px-8"
 				outerClassName="bg-secondary mt-16"
 			>
-				<h2 className="text-base font-semibold tracking-wide text-orange-600 uppercase">
+				<h2 className="text-base font-semibold tracking-wide text-orange-600 uppercase dark:text-sky-500">
 					AI-powered text summarization
 				</h2>
 				<p className="mt-1 text-4xl font-extrabold sm:text-5xl sm:tracking-tight lg:text-6xl">
@@ -193,7 +195,7 @@ export default function Home() {
 						Free, easy to use, and no ads.
 					</h1>
 
-					<div className="text-lg text-center text-gray-900 sm:text-end">
+					<div className="text-lg text-center sm:text-end">
 						With Balladeer, you can get the gist of the book in
 						minutes instead of hours. You can also use Balladeer to
 						summarize your notes and lectures.
@@ -201,7 +203,7 @@ export default function Home() {
 
 					<a
 						href="#"
-						className="mt-2 ml-auto text-lg font-semibold text-orange-500 sm:ml-0"
+						className="mt-2 ml-auto text-lg font-semibold text-orange-500 dark:text-sky-500 sm:ml-0"
 					>
 						Get Started &rarr;
 					</a>
@@ -209,7 +211,7 @@ export default function Home() {
 
 				<Image
 					src="/images/surreal-flying-bulbs.svg"
-					className="mx-auto"
+					className="mx-auto dark:invert"
 					alt="surreal-flying-bulbs"
 					height={300}
 					width={300}
