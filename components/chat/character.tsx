21b491e0cc0048532e36437f83c500dd07618ba5
Kevin Liu <66856750+Kevin-Liu-01@users.noreply.github.com>
2023-06-04 00:35:12 -0400

    add carousel

--- components/chat/character.tsx
+++ components/chat/character.tsx
@@ -1,27 +1,62 @@
 "use client";
 
 import { Content } from "@/components/content";
+import useEmblaCarousel from "embla-carousel-react";
+import { useEffect, useCallback } from "react";
+import { User } from "lucide-react";
 
 function Character(props: any) {
+	const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false });
+
+	useEffect(() => {
+		if (emblaApi) {
+			console.log(emblaApi.slideNodes()); // Access API
+		}
+	}, [emblaApi]);
+
+	const scrollPrev = useCallback(() => {
+		if (emblaApi) emblaApi.scrollPrev();
+	}, [emblaApi]);
+
+	const scrollNext = useCallback(() => {
+		if (emblaApi) emblaApi.scrollNext();
+	}, [emblaApi]);
+
 	return (
 		<>
 			<Content
 				as="section"
-				className="relative p-4 overflow-hidden max-w-7xl bg-orange-500 dark:bg-sky-500 rounded-md grid grid-cols-4 gap-4"
+				className="relative py-4 px-0 overflow-hidden max-w-7xl bg-orange-500 dark:bg-sky-500 rounded-md  "
 			>
-				{props.characters.map((character: any) => (
-					<div
-						key={character.name}
-						className="relative z-10 bg-secondary rounded-md p-4 shadow-lg"
-					>
-						<p className="mt-1 text-xl font-extrabold sm:text-2xl sm:tracking-tight  ">
-							{character.name}
-						</p>
-						<p className=" mt-2 text-base  ">
-							{character.description}
-						</p>
+				<div className="embla overflow-hidden w-full " ref={emblaRef}>
+					<div className="embla__container px-4 flex flex-row gap-4">
+						{props.characters.map((character: any) => (
+							<div
+								key={character.name}
+								className=" embla__slide min-w-0 relative flex-[0_0_100%] justify-center bg-secondary rounded-md p-4 py-8 shadow-lg"
+							>
+								<p className="mt-1 text-xl font-extrabold sm:text-2xl sm:tracking-tight flex flex-row items-center gap-2 ">
+									<User /> {character.name}
+								</p>
+								<p className=" mt-2 text-base  ">
+									{character.description}
+								</p>
+							</div>
+						))}{" "}
 					</div>
-				))}
+					<button
+						className="embla__prev bg-background rounded-md p-2 absolute top-1/2 left-2 transform -translate-y-1/2 z-10 "
+						onClick={scrollPrev}
+					>
+						Prev
+					</button>
+					<button
+						className="embla__next bg-background rounded-md p-2 absolute top-1/2 right-2 transform -translate-y-1/2 z-10 "
+						onClick={scrollNext}
+					>
+						Next
+					</button>
+				</div>
 			</Content>
 		</>
 	);
