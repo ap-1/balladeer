81bc7f85a50adce9317488ccba0387933e543c1d
Kevin Liu <66856750+Kevin-Liu-01@users.noreply.github.com>
2023-06-03 20:43:11 -0400

    chat page

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -16,35 +16,49 @@ export default function Home() {
 		<>
 			<Navbar currentTitle="Home" />
 			<Content
-				as="heading"
-				className="flex flex-col gap-3 my-16 sm:gap-0 sm:flex-row"
+				as="section"
+				className="px-4 py-16 mx-auto text-center flex justify-center max-w-7xl sm:px-0 relative overflow-hidden"
+				outerClassName="bg-orange-500"
 			>
-				<div className="flex items-center max-w-lg">
-					<div className="flex flex-col gap-y-3">
-						<h1 className="font-sans text-6xl font-black">
-							Studying just got smarter.
-						</h1>
-						<h2 className="text-xl">
-							Create concise and insightful book summaries in
-							minutes with AI-powered text summarization.
-						</h2>
-
-						<div className="flex items-center w-full max-w-full mt-3 space-x-2 sm:max-w-sm">
-							<Input
-								type="text"
-								placeholder="Summarize a book right now!"
-							/>
-						</div>
-					</div>
+				<div className="relative z-10 dark:text-white text-primary">
+					<p className="mt-1 text-4xl font-extrabold sm:text-5xl sm:tracking-tight lg:text-6xl">
+						Hamlet
+					</p>
+					<p className="text-base font-semibold tracking-wide border-2 p-2 mt-4 border-orange-900 dark:border-orange-100 uppercase rounded-md">
+						William Shakespeare
+					</p>
 				</div>
-
-				<Image
-					src="/images/student-going-to-school.svg"
-					alt="student-going-to-school"
-					className="mx-auto dark:invert"
-					height={400}
-					width={400}
-				></Image>
+			</Content>
+			<Content
+				as="section"
+				className="px-4 flex flex-row items-center max-w-7xl text-lg relative "
+				outerClassName="border-b border-border"
+			>
+				<div className="py-4 mr-4">Jump to:</div>
+				<button className="hover:bg-secondary duration-150 h-full px-5 py-4">
+					Summary
+				</button>
+				<button className="hover:bg-secondary duration-150 h-full px-5 py-4">
+					Characters
+				</button>
+				<button className="hover:bg-secondary duration-150 h-full px-5 py-4">
+					Literary Devices
+				</button>
+				<button className="hover:bg-secondary duration-150 h-full px-5 py-4">
+					Q&A
+				</button>
+			</Content>
+			<Content
+				as="section"
+				className="px-4 py-8 flex flex-row items-center max-w-7xl text-lg relative "
+				outerClassName="border-b border-border"
+			>
+				First performed around 1600, Hamlet tells the story of a prince
+				whose duty to revenge his father’s death entangles him in
+				philosophical problems he can’t solve. Shakespeare’s best-known
+				play is widely regarded as the most influential literary work
+				ever written. Read a character analysis of Hamlet, plot summary,
+				and important quotes.
 			</Content>
 		</>
 	);
