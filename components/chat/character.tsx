3b98d9afd50cc57b3907e6beb3a178c6464beb35
Kevin Liu <66856750+Kevin-Liu-01@users.noreply.github.com>
2023-06-04 00:49:23 -0400

    update carousel

--- components/chat/character.tsx
+++ components/chat/character.tsx
@@ -3,10 +3,17 @@
 import { Content } from "@/components/content";
 import useEmblaCarousel from "embla-carousel-react";
 import { useEffect, useCallback } from "react";
-import { User } from "lucide-react";
+import { User, ArrowLeft, ArrowRight } from "lucide-react";
+import Autoplay from "embla-carousel-autoplay";
+import AutoHeight from "embla-carousel-auto-height";
+import { WheelGesturesPlugin } from "embla-carousel-wheel-gestures";
 
 function Character(props: any) {
-	const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false });
+	const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [
+		Autoplay(),
+		AutoHeight(),
+		WheelGesturesPlugin(),
+	]);
 
 	useEffect(() => {
 		if (emblaApi) {
@@ -29,11 +36,11 @@ function Character(props: any) {
 				className="relative py-4 px-0 overflow-hidden max-w-7xl bg-orange-500 dark:bg-sky-500 rounded-md  "
 			>
 				<div className="embla overflow-hidden w-full " ref={emblaRef}>
-					<div className="embla__container px-4 flex flex-row gap-4">
+					<div className="embla__container items-start px-4 flex flex-row gap-4">
 						{props.characters.map((character: any) => (
 							<div
 								key={character.name}
-								className=" embla__slide min-w-0 relative flex-[0_0_100%] justify-center bg-secondary rounded-md p-4 py-8 shadow-lg"
+								className=" embla__slide min-w-0 relative flex-[0_0_100%] sm:flex-[0_0_50%] justify-center bg-secondary rounded-md p-8 shadow-lg"
 							>
 								<p className="mt-1 text-xl font-extrabold sm:text-2xl sm:tracking-tight flex flex-row items-center gap-2 ">
 									<User /> {character.name}
@@ -45,16 +52,16 @@ function Character(props: any) {
 						))}{" "}
 					</div>
 					<button
-						className="embla__prev bg-background rounded-md p-2 absolute top-1/2 left-2 transform -translate-y-1/2 z-10 "
+						className="embla__prev bg-background hover:bg-orange-500 hover:dark:bg-sky-500 duration-150 rounded-full p-2 absolute top-1/2 left-2 transform -translate-y-1/2 z-10 "
 						onClick={scrollPrev}
 					>
-						Prev
+						<ArrowLeft />
 					</button>
 					<button
-						className="embla__next bg-background rounded-md p-2 absolute top-1/2 right-2 transform -translate-y-1/2 z-10 "
+						className="embla__next bg-background hover:bg-orange-500 hover:dark:bg-sky-500 duration-150 rounded-full p-2 absolute top-1/2 right-2 transform -translate-y-1/2 z-10 "
 						onClick={scrollNext}
 					>
-						Next
+						<ArrowRight className="text-primary" />
 					</button>
 				</div>
 			</Content>
